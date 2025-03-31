7900762ec15664c952592861ecf3cd42d69a0ec4
witvick <nitishkumar37373@gmail.com>
2025-03-31 10:17:07 +0530

    Project 2 Fkiles Baseline Project

---
+++ Java_Baseline_project/src/CLasses/Class2java.java
@@ -0,0 +1,10 @@
+package CLasses;
+
+public class Class2java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java_Baseline_project/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Java_Baseline_project {
+}
